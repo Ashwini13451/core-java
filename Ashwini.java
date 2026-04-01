7394d3a6e639adc3a659bc105026ebfd23cf495b
Ashwini13451 <ap8247274@gmail.com>
2026-04-01 21:13:26 +0530

    reference

---
+++ Ashwini.java
@@ -0,0 +1,21 @@
+class Ashwini{
+public static void main(String[] args){
+	{
+	}
+	
+	
+	
+
+String Properties[]={"Karnataka", "Kerala", "Tamilanadu"};
+  // Properties[] = {"KR Puram"};
+  //for each 
+  for(String Property:Properties)
+  System.out.println(Property);
+  
+ 
+  
+  
+  
+  
+}
+}
